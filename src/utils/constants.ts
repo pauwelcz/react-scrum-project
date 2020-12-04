7edafcf63f44c56bd4ce9c9245d85e47a8d5427c
Adam Radvan <radvan@inqool.cz>
2020-12-04 21:57:09 +0100

    filtering of tasks

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const PALLETE = {
+  red: '#ff0000',
+  blue: '#0000ff',
+  green: '#00ff00',
+  yellow: 'yellow',
+  cyan: 'cyan',
+  lime: 'lime',
+  gray: 'gray',
+  orange: 'orange',
+  lightpurple: '#dd33fa',
+  white: 'white',
+  pink: 'pink',
+  lightblue: 'lightblue',
+};
